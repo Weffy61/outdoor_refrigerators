396169e42f463a22610431adfb8b7d1aac437123
Weffy61 <weffy@mail.ru>
2024-05-21 22:17:24 +0300

    Reports page is done

--- report/views.py
+++ report/views.py
@@ -2,6 +2,7 @@ from urllib.parse import urljoin
 
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 from report.models import Refrigerator, Report
@@ -47,7 +48,22 @@ def index(request):
 def get_reports(request):
     current_user = request.user
     base_report_url = urljoin(request.build_absolute_uri('/'), 'report/')
-    reports = [
+    search_query = request.GET.get('q', '').strip().lower()
+    reports = Report.objects.filter(sender=current_user).select_related('refrigerator')
+    status_mapping = {
+        'на рассмотрении': 'on_review',
+        'одобрен': 'approved',
+        'отклонен': 'decline'
+    }
+    if search_query:
+        status_query = status_mapping.get(search_query)
+        reports = reports.filter(
+            Q(refrigerator__organization__icontains=search_query) |
+            Q(refrigerator__organization_address__icontains=search_query) |
+            Q(date__icontains=search_query) |
+            Q(status__icontains=status_query) if status_query else Q()
+        )
+    reports_statistic = [
         {
             'organization': report.refrigerator.organization,
             'organization_address': report.refrigerator.organization_address,
@@ -55,7 +71,7 @@ def get_reports(request):
             'status': report.status,
             'url': urljoin(base_report_url, str(report.pk))
 
-        } for report in Report.objects.filter(sender=current_user).select_related('refrigerator')
+        } for report in reports
     ]
     user = {
         'name': f'{current_user.first_name} {current_user.last_name}',
@@ -63,7 +79,7 @@ def get_reports(request):
         'photo': request.build_absolute_uri(current_user.photo.url) if current_user.photo else None
     }
 
-    paginator = Paginator(reports, 10)
+    paginator = Paginator(reports_statistic, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     return render(
@@ -71,6 +87,7 @@ def get_reports(request):
         'report/reports.html',
         context={
             'page': page,
-            'user': user
+            'user': user,
+            'search_query': search_query
         }
     )
