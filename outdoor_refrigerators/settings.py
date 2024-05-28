5417a9e8a846379caeb7ceb91e51494c07ae544b
Weffy61 <weffy@mail.ru>
2024-05-28 18:26:24 +0300

    Add db url

--- outdoor_refrigerators/settings.py
+++ outdoor_refrigerators/settings.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from environs import Env
 
 env = Env()
@@ -63,10 +65,7 @@ WSGI_APPLICATION = 'outdoor_refrigerators.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
